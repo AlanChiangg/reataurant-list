13404179f569d143d2494c256583467af126418b
AlanChiangg <alaln35alan@gmail.com>
2023-06-11 19:40:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 // require packages used in the project
 const express = require('express')
+const mongoose = require('mongoose') // 載入mongoose
+
+// 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+});
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
