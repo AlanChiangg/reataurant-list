1ebb5f664427c48e360f3d472a080ae6fea1d95d
AlanChiangg <alaln35alan@gmail.com>
2023-07-13 22:16:14 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
-const routes = require('./routes')
 const session = require('express-session')
-const usePassport = require('./config/passport')
 const flash = require('connect-flash')
-
+const PORT = process.env.PORT || 3000
+const usePassport = require('./config/passport')
+const routes = require('./routes')
 require('./config/mongoose')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
-const port = 3000
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -35,7 +38,6 @@ app.use((req, res, next) => {
 
 app.use(routes)
 
-// start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
